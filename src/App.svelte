436a1561a298d8dea819f0e9b4213641face4a8a
amiter123king <amiter123king@gmail.com>
2021-04-23 12:14:46 +0300

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import {
+		Card,
+		CardTitle,
+		CardSubtitle,
+		CardActions,
+		Button,
+    	Icon,
+		MaterialApp,
+ListItem
+	} from 'svelte-materialify';
+import Law from './Law.svelte';
+
+</script>
+
+<MaterialApp>
+	<Law id={4} content="תוכן חוק" />
+	<Law id={5} content="בזיון" />
+</MaterialApp>
---
+++ src/Law.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { Button, Card, CardActions, CardSubtitle, CardTitle, Icon } from "svelte-materialify";
+
+export let law;
+</script>
+
+<style></style>
+
+<Card style="max-width: 500px; margin: 10px">
+<CardTitle>{law.contentstring}</CardTitle>
+    <CardSubtitle>LAW #{law.id}</CardSubtitle>
+    <CardActions>
+        
+      <Button text>FOR</Button>
+      <Button text>ABSTAIN</Button>
+      <Button text>AGAINST</Button>
+    </CardActions>
+</Card>
