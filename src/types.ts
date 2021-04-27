65961c50a2c2b2ce6d428859e4fbc2d1e0425a27
Amit Nave <amiter123king@gmail.com>
2021-04-27 11:25:54 +0300

    added routing, made laws component better

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+
+    export interface Member {
+        id: number;
+        name: string;
+        phone: string;
+        president: boolean;
+        registered: boolean;
+        joined: Date;
+    }
+
+    export interface Content {
+        id: number;
+        type: string;
+        description: string;
+    }
+
+    export interface Voter {
+        id: number;
+        name: string;
+        phone: string;
+        president: boolean;
+        registered: boolean;
+        joined: Date;
+    }
+
+    export interface Vote {
+        id: number;
+        voter: Voter;
+        vote: string;
+        reason: string;
+    }
+
+    export type Status = "PASSED" | "UNDER_VOTE" | "FAILED" | "VETOED" | "CANCELED";
+
+    export interface Law {
+        id: number;
+        legislator: Member;
+        content: Content;
+        votes: Vote[];
+        status: Status;
+        timestamp: Date;
+        constitutional: boolean;
+        anonymousLegislator: boolean;
+        fakeName: string;
+        contentString: string;
+    }
+
