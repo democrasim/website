74bb68fa58d7ba028114e97ebf9879304a03c23c
Amit Nave <amiterdc@gmail.com>
2021-05-10 18:42:55 +0300

    Delete .svelte-kit directory

--- .svelte-kit/output/client/_app/chunks/stores-e76e5349.js
+++
@@ -1,1 +0,0 @@
-import{C as s}from"./vendor-2294d25b.js";const o=s("light"),t=s([]),a=s([]);export{t as l,a as m,o as t};
--- .svelte-kit/output/client/_app/pages/user/[id].svelte-31b02b98.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as r,s as t}from"../../chunks/vendor-2294d25b.js";export default class extends s{constructor(s){super(),r(this,s,null,null,t,{})}}
