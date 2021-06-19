db82bde640a418613f47332d23d5c446a099d8db
Amit Nave <amitnave@nucleaimd.com>
2021-06-19 06:49:19 +0300

    ✊🏻fixed bugs and adapted site to server

---
+++ src/routes/Legislate.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { List, TextField } from "svelte-materialify";
+
+  let name = "";
+</script>
+
+<h3>Legislating today, are we? let's begin!</h3>
+
+<TextField bind:value={name}>Law name (optional)</TextField>
+
+<List class="elevation-2" />
